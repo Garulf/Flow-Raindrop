178a49cbb7ffec2de4d36462267c0b659235f109
Garulf <535299+Garulf@users.noreply.github.com>
2024-02-16 03:37:42 -0500

    Update plugin

--- plugin/main.py
+++ plugin/main.py
@@ -1,29 +1,49 @@
+import shutil
+from typing import List
+
 from pyflowlauncher import Plugin, ResultResponse, send_results
 from pyflowlauncher.settings import settings
-from pyflowlauncher.utils import score_results
+from raindrop import Raindrop
+
+from results import query_results, context_menu_results, init_results
 
 
-from raindropio import API, CollectionRef
-import requests_cache
+CACHE_TTL = {
+    "5 minutes": 300,
+    "15 minutes": 900,
+    "30 minutes": 1800,
+    "1 hour": 3600,
+    "6 hours": 21600,
+    "12 hours": 43200,
+    "1 day": 86400,
+    "3 days": 259200,
+}
 
-from results import query_results
+DEFAULT_CACHE_TTL = CACHE_TTL["5 minutes"]
+
 
 plugin = Plugin()
 
 
 @plugin.on_method
 def query(query: str) -> ResultResponse:
-    requests_cache.install_cache(".cache", backend="sqlite", expire_after=300)
     api_token = settings().get('api_token')
-    rain_api = API(api_token)
-    collection_setting = settings().get('default_collection', 'All Collections')
-    collection = {
-        "All Collections": CollectionRef({"$id": 0}),
-        "Unsorted": CollectionRef.Unsorted
-    }.get(collection_setting)
+    cache_ttl = settings().get('cache_ttl', DEFAULT_CACHE_TTL)
+    if not api_token:
+        return send_results([init_results()])
+    if not query:
+        return send_results([])
+    rd = Raindrop(api_token, CACHE_TTL[cache_ttl])
     return send_results(
-        score_results(
-            query,
-            query_results(rain_api, collection),
-        )
+        query_results(rd, query)
     )
+
+
+@plugin.on_method
+def context_menu(data: List) -> ResultResponse:
+    return send_results(context_menu_results())
+
+
+@plugin.on_method
+def clear_cache() -> None:
+    shutil.rmtree('.cache')
--- plugin/results.py
+++ plugin/results.py
@@ -2,28 +2,55 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Generator
 
 from pyflowlauncher import Result
-from pyflowlauncher.api import open_url
-
-from raindropio import Raindrop
+from pyflowlauncher.api import open_url, open_setting_dialog
+from pyflowlauncher.icons import RECYCLEBIN, SETTINGS, LINK
 
 
 if TYPE_CHECKING:
-    from raindropio import API, CollectionRef
+    from raindrop import Raindrop, Item
 
 
-def query_result(raindrop: Raindrop) -> Result:
+def init_results() -> Result:
     return Result(
-        Title=raindrop.title,
-        SubTitle=raindrop.excerpt,
-        IcoPath=raindrop.cover,
-        JsonRPCAction=open_url(raindrop.link)
+        Title="No API token found!",
+        SubTitle="Please enter your Raindrop.io API token in plugin settings.",
+        IcoPath=SETTINGS,
+        JsonRPCAction=open_setting_dialog()
+    )
+
+
+def query_result(item: Item) -> Result:
+    return Result(
+        Title=item["title"],
+        SubTitle=item["excerpt"],
+        IcoPath=item["cover"],
+        ContextData=[],
+        JsonRPCAction=open_url(item["link"])
     )
 
 
 def query_results(
-    rain_api: API,
-    collection: CollectionRef,
+    raindrop: Raindrop,
+    query: str
 ) -> Generator[Result, None, None]:
-    search = Raindrop.search(rain_api, collection=collection)
+    search = raindrop.search(query)
     for item in search:
         yield query_result(item)
+
+
+def context_menu_results() -> Generator[Result, None, None]:
+    yield Result(
+        Title="Open Raindrop.io",
+        SubTitle="Open Raindrop.io in your browser",
+        IcoPath=LINK,
+        JsonRPCAction=open_url("https://raindrop.io")
+    )
+    yield Result(
+        Title="Clear cached data",
+        SubTitle="All cached data will be removed",
+        IcoPath=RECYCLEBIN,
+        JsonRPCAction={
+            "method": "clear_cache",
+            "parameters": [],
+        }
+    )
